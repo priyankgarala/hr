342f360acf7b2d309f024f047c374ef26b795ead
chater <priyankgarala123@gmail.com>
2026-03-28 20:35:46 +0530

    api changes

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -2,7 +2,7 @@ import axios from "../utils/axios";
 
 export const registerUser = (data) =>
   axios.post("/auth/register", data);
-
+// 🔥 Login and profile APIs
 export const loginUser = (data) =>
   axios.post("/auth/login", data);
 
